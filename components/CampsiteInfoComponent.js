66dc3d8cacdc24906c837c95b125497c8ae7e21a
mejowett <78455126+mejowett@users.noreply.github.com>
2021-08-09 09:07:12 -0400

    task 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -32,10 +32,9 @@ const mapDispatchToProps = {
 
 function RenderCampsite(props) {
   const { campsite } = props;
-
   const view = React.createRef();
-
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -68,6 +67,8 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     },
